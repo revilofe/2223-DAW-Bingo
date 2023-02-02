338ec05d88a4ab6535c8fd4f964a756dc137c293
Edu <revilofe@gmail.com>
2023-02-02 19:48:34 +0100

    Test JuegoTest.kt

---
+++ src/test/kotlin/es/edu/iesra/daw/revilofe/bingo/JuegoTest.kt
@@ -0,0 +1,29 @@
+package es.edu.iesra.daw.revilofe.bingo
+
+import io.kotest.core.spec.style.DescribeSpec
+
+/*
+El juego consistirá en lo siguiente:
+
+Configura tu juego: generar un número X de cartones.
+Iterar la dinámica siguiente hasta que algún cartón consiga bingo.
+    El bombo genera una bola marcada con un número.
+    Se marca en los X cartones el número marcado en la bola.
+    Se chequea si algún cartón de los X cartones, ha conseguido línea.
+    Se chequea si algún cartón de los X cartones, ha conseguido bingo.
+Resume el juego.
+
+Cada vez que se consiga una línea, se mostrará el identificador del
+cartón y las posiciones y número de cartones marcados para conseguir la línea.
+Cuando se consiga una línea, se mostrará el identificador del cartón
+y las posiciones y número de cartones marcados.
+Al finalizar, se podrá pedir un resumen por cartón, en el que se muestran
+los identificadores de los cartones y las líneas conseguidas, así como
+el bingo del cartón ganador.
+
+
+ */
+
+class JuegoTest : DescribeSpec(){
+
+}
