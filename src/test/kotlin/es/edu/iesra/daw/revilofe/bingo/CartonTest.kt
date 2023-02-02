b8f53853a52dfa88a50610ea58668b115be7fa09
Edu <revilofe@gmail.com>
2023-02-02 19:45:35 +0100

    Test Cartón

---
+++ src/test/kotlin/es/edu/iesra/daw/revilofe/bingo/CartonTest.kt
@@ -0,0 +1,16 @@
+package es.edu.iesra.daw.revilofe.bingo
+
+import io.kotest.core.spec.style.DescribeSpec
+
+/*
+Cada cartón tiene que gestionar los números que han salido y coinciden con un
+número que existe en el cartón. Es decir, tiene que marcar aquellos números que
+han salido.
+El cartón tiene que poder chequear si ha conseguido marcar una línea de 4
+números  (línea recta, horizontal, vertical o diagonal)
+El cartón tiene que poder chequear si ha conseguido bingo, si ha
+conseguido marcar todos los números del cartón.
+ */
+class CartonTest : DescribeSpec(){
+
+}
