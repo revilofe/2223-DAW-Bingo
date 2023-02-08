8aadee1025c4db69ac6ecc3201baae4458313e23
Edu <revilofe@gmail.com>
2023-02-08 19:01:42 +0100

    convierto en singlenton Juego

--- src/main/kotlin/es/edu/iesra/daw/revilofe/bingo/Locutor.kt
+++ src/main/kotlin/es/edu/iesra/daw/revilofe/bingo/Locutor.kt
@@ -6,7 +6,7 @@ import es.edu.iesra.daw.revilofe.patrones.signalslot.Signal
 Los locutores de bingo supervisan las partidas de bingo que se juegan en los clubes de bingo.
 Llaman a los números hasta que se produce un ganador.
  */
-class Locutor(val bombo:Bombo = Bombo ) {
+object Locutor(val bombo:Bombo = Bombo ) {
     val nuevoNumero = Signal<Int>()
     fun sacaNumero():Boolean{
         var hayBolas = false
