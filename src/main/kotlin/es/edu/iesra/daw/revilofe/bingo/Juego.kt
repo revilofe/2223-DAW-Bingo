48c6f14056d991e0c4defc2bb1b08957886dbde7
edu <revilofe@gmail.com>
2023-02-08 23:52:33 +0100

    avance en juegos

--- src/main/kotlin/es/edu/iesra/daw/revilofe/bingo/Juego.kt
+++ src/main/kotlin/es/edu/iesra/daw/revilofe/bingo/Juego.kt
@@ -1,12 +1,24 @@
 package es.edu.iesra.daw.revilofe.bingo
 
-data class Config(val param: Int = 0)
+data class ConfigJuego(val nunCartones: Int = 5, val locutor: Locutor =Locutor, val generador: GeneradorCartones = GeneradorCartones)
+
 object Juego {
 
+    private var numCartones: Int = 0
+    private lateinit var locutor: Locutor
+    private lateinit var generador: GeneradorCartones
 
-    fun configure(config : Config) {
+    fun configura(config : ConfigJuego= ConfigJuego()) {
+        numCartones = config.nunCartones
+        locutor = config.locutor
+        generador = config.generador
 
     }
+
+    fun montaJuego(){
+
+    }
+
     fun play(){
 
     }
