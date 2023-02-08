f61de48fb12aec4224c1d3e47b7a3fc533477b1e
edu <revilofe@gmail.com>
2023-02-09 00:08:04 +0100

    Metodo genera

--- src/main/kotlin/es/edu/iesra/daw/revilofe/bingo/GeneradorCartones.kt
+++ src/main/kotlin/es/edu/iesra/daw/revilofe/bingo/GeneradorCartones.kt
@@ -1,5 +1,20 @@
 package es.edu.iesra.daw.revilofe.bingo
 
+import es.edu.iesra.daw.revilofe.servicios.ServicioDeNumeros
+
 object GeneradorCartones {
+    fun genera(cuantos: Int): List<Carton> {
+        return List<Carton>(cuantos){
+            Carton( it.toString(),
+                listOf(
+                    ServicioDeNumeros(1,16).siguientesNumeros(4),
+                    ServicioDeNumeros(16,31).siguientesNumeros(4),
+                    ServicioDeNumeros(31,46).siguientesNumeros(4),
+                    ServicioDeNumeros(46,61).siguientesNumeros(4),
+                    ServicioDeNumeros(61,76).siguientesNumeros(4)
+                )
+            )
+        }
+    }
 
 }
